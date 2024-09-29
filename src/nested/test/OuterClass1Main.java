32acd91d9f10b645ae1d0ddce8b1e756cc59289d
una <una_j@naver.com>
2024-09-30 00:16:09 +0900

    ~ 세션 12 - 끝

---
+++ src/nested/test/OuterClass1Main.java
@@ -0,0 +1,9 @@
+package nested.test;
+
+public class OuterClass1Main {
+
+    public static void main(String[] args) {
+        OuterClass1.NestedClass nested = new OuterClass1.NestedClass();
+        nested.hello();
+    }
+}
